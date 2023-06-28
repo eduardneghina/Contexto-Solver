7c932b3a804bb03017a03c1deb90b447090bc9d1
eduardneghina <41010274+eduardneghina@users.noreply.github.com>
2023-06-28 22:53:06 +0300

    extract_all_history_games_function yey!

--- Extractor.py
+++ Extractor.py
@@ -8,8 +8,8 @@ import time
 from WebController import *
 
 class Extractor:
-    def __init__(self):
-        pass
+    def __init__(self, driver):
+        self._driver = driver
 
     def save(self, path):
         json.dump(self.data, open(path, 'w'))
@@ -18,17 +18,31 @@ class Extractor:
         self.data = json.load(open(path))
 
     def extract_game_data(self, game_number, driver):
-        webcontrol = WebController(driver)
+        webcontrol = WebController(self._driver)
         webcontrol.click_desired_previous_games(game_number)
         webcontrol.click_give_up()
         time.sleep(3)
         webcontrol.click_closest_word()
         time.sleep(3)
         data = []
-        elements = driver.find_elements(By.CLASS_NAME, 'row-wrapper')
+        elements = self._driver.find_elements(By.CLASS_NAME, 'row-wrapper')
         for e in elements:
             data.append(e.text.split('\n'))
         # filtru duplicate
         res = []
         [res.append(x) for x in data if x not in res]
         return res
+
+    def extract_all_history_games(self): # WARNING ! Incepe de la jocul 1 pana la ultimul si scoate toate datele . DEMO #1 - PLEASE CHECK DEMOS FOLDERS
+        webcontrol = WebController(self._driver)
+        time.sleep(1)
+        a = self._driver.find_element(By.CLASS_NAME, 'info-bar')
+        gamenr = a.text[7:10]
+        for i in range(int(gamenr)):
+            data = self.extract_game_data(i+1, self._driver)
+            print(data)
+            time.sleep(1)
+            b = self._driver.find_element(By.CLASS_NAME, "modal-close-button")
+            b.click()
+            time.sleep(1)
+
--- main.py
+++ main.py
@@ -4,8 +4,5 @@ from Database import *
 from Extractor import *
 
 driver = initiate_contexto()
-extract = Extractor()
-a = extract.extract_game_data(40, driver)
-print(a)
-# output will be the list from game 40 : ['potato', '1'], ['mash', '2'], ['carrot', '3'], ['onion', '4'], ['vegetable', '5'], ..etc
-time.sleep(10)
+extractor = Extractor(driver)
+extractor.extract_all_history_games()
