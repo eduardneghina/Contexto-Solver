8caebf5a0e75d84760f4efb3e26c24146c0bb613
eduardneghina <41010274+eduardneghina@users.noreply.github.com>
2023-06-29 13:59:34 +0300

    bug fix + extract old data example

--- Extractor.py
+++ Extractor.py
@@ -10,12 +10,7 @@ from WebController import *
 class Extractor:
     def __init__(self, driver):
         self._driver = driver
-
-    def save(self, path):
-        json.dump(self.data, open(path, 'w'))
-
-    def load(self, path):
-        self.data = json.load(open(path))
+        self.data = None
 
     def extract_game_data(self, game_number, driver):
         webcontrol = WebController(self._driver)
@@ -33,16 +28,49 @@ class Extractor:
         [res.append(x) for x in data if x not in res]
         return res
 
-    def extract_all_history_games(self): # WARNING ! Incepe de la jocul 1 pana la ultimul si scoate toate datele . DEMO #1 - PLEASE CHECK DEMOS FOLDERS
+    def extract_all_history_games(self): # WARNING ! Incepe de la jocul 1 pana la ultimul si scoate toate datele . DEMO #1 - PLEASE CHECK DEMOS FOLDERS # ~4 games/min save rate
         webcontrol = WebController(self._driver)
         time.sleep(1)
         a = self._driver.find_element(By.CLASS_NAME, 'info-bar')
         gamenr = a.text[7:10]
         for i in range(int(gamenr)):
-            data = self.extract_game_data(i+1, self._driver)
-            print(data)
             time.sleep(1)
-            b = self._driver.find_element(By.CLASS_NAME, "modal-close-button")
-            b.click()
-            time.sleep(1)
+            try:
+                data = self.extract_game_data(i+1, self._driver)
+                print(data)
+            except:
+                print("e gol lol")
+                pass
+            else:
+                b = self._driver.find_element(By.CLASS_NAME, "modal-close-button")
+                b.click()
+                time.sleep(1)
+                text = TextData()
+                datafile = text.load_data()
+                if datafile is None:
+                    text.save_data(str(data))
+                else:
+                    text.add_data(str(data))
 
+
+class TextData(Extractor):
+    def __init__(self):  # Hardcoded - path variable should be as a param.
+        self.path = "C:\\Contexto\\data.txt"
+
+    def load_data(self):
+        try:
+            with open(self.path, "r") as infile:
+                data = infile.read()
+            return data
+        except FileNotFoundError:
+            print("File not found.")
+            return None
+
+    def save_data(self, data):
+        with open(self.path, "w") as outfile:
+            outfile.write(data)
+
+    def add_data(self, new_data):
+        data = self.load_data()
+        data += "\n" + new_data
+        self.save_data(data)
