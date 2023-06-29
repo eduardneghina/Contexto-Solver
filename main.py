b3c29cf3e4edb0ab37eb5b6e158c32a6d6f12cf8
eduardneghina <41010274+eduardneghina@users.noreply.github.com>
2023-06-29 22:13:56 +0300

    Skeleton for user  options

--- main.py
+++ main.py
@@ -2,7 +2,28 @@ import time
 from Database import *
 from Extractor import *
 from AI import *
+import argparse
 
-driver = initiate_contexto()
-controller = WebController(driver)
+def main():
+    while True:
+        driver = initiate_contexto()
+        controller = WebController(driver)
+        print("Choose an option:")
+        print("1. ")
+        print("2. ")
+        print("3. ")
+        print("0. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "0":
+            break
+        else:
+            print("Invalid choice")
 
+if __name__ == "__main__":
+    main()
